5d184bda2aa05b86ff444d401bf0d67c7e43049c
Flavius <Flavius256@icloud.com>
2015-05-08 14:54:57 -0600

    Created MYSQL.js

---
+++ server/js/db_providers/mysql.js
@@ -0,0 +1,13 @@
+var cls = require("./lib/class"),
+    Player = require("./player"),
+    mysql = require("mysql");
+    Messages = require("../message"),
+    bcrypt = require("bcrypt"),
+    inventory = require("../inventory");
+    
+  module.exports = Mysql = cls.Class.extend({
+    init() {
+  
+  
+    }
+  });
