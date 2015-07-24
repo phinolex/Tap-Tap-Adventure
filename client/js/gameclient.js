4b8ada6964ae6c7a293c500dfa0f2304120e604f
Tap Tap Adventure <taptapadventure@gmail.com>
2015-07-24 02:06:01 -0600

    Re-Enabled HTTPS

--- client/js/gameclient.js
+++ client/js/gameclient.js
@@ -73,7 +73,7 @@ define(['player', 'entityfactory', 'lib/bison'], function(Player, EntityFactory,
         },
 
         connect: function(dispatcherMode) {
-            var url = "ws://"+ this.host +":"+ this.port +"/",
+            var url = "wss://"+ this.host +":"+ this.port +"/",
                 self = this;
 
             log.info("Trying to connect to server : "+url);
