a83573cb660b96a284f89f1347e2df340c0bada7
Jade Krafsig <jade@design1online.com>
2018-12-09 15:05:22 -0500

    Updating pointer graphic

--- client/js/controllers/pointer.js
+++ client/js/controllers/pointer.js
@@ -1,210 +1,214 @@
 define(['jquery', '../renderer/pointers/pointer'], function($, Pointer) {
+return Class.extend({
 
-    return Class.extend({
+    init: function(game) {
+      var self = this;
 
-        init: function(game) {
-            var self = this;
+      self.game = game;
+      self.pointers = {};
+      self.scale = self.getScale();
+      self.container = $('#bubbles');
+    },
 
-            self.game = game;
-            self.pointers = {};
+    create: function(id, type) {
+      var self = this;
 
-            self.scale = self.getScale();
+      if (id in self.pointers) {
+        return;
+      }
 
-            self.container = $('#bubbles');
-        },
+      var element = $('<div id="' + id + '" class="pointer"></div>');
 
-        create: function(id, type) {
-            var self = this;
+      self.setSize(element);
 
-            if (id in self.pointers)
-                return;
+      self.container.append(element);
 
-            var element = $('<div id="' + id +'" class="pointer"></div>');
+      self.pointers[id] = new Pointer(id, element, type);
+    },
 
-            self.setSize(element);
+    resize: function() {
+      var self = this;
 
-            self.container.append(element);
+      _.each(self.pointers, function(pointer) {
 
-            self.pointers[id] = new Pointer(id, element, type);
-        },
-
-        resize: function() {
-            var self = this;
-
-            _.each(self.pointers, function(pointer) {
-
-                switch (pointer.type) {
-
-                    case Modules.Pointers.Relative:
-
-                        var scale = self.getScale(),
-                            x = pointer.x,
-                            y = pointer.y,
-                            offsetX = 0,
-                            offsetY = 0;
-
-                        if (scale === 1) {
-                            offsetX = pointer.element.width() / 2 + 5;
-                            offsetY = pointer.element.height() / 2 - 4;
-                        }
-
-                        pointer.element.css('left', (x * scale) - offsetX + 'px');
-                        pointer.element.css('top', (y * scale) - offsetY + 'px');
-
-                        break;
-                }
-
-            });
-        },
-
-        setSize: function(element) {
-            var self = this;
-
-            self.updateScale();
-
-            element.css({
-                'width': (16 + 16 * self.scale) + 'px',
-                'height': (16 + 16 * self.scale) + 'px',
-                'margin': 'inherit',
-                'margin-top': '-' + (6 * self.scale) + 'px',
-                'top': (10 * self.scale) + 'px',
-                'background': 'url("img/' + self.scale + '/pointer.png")'
-            });
-        },
-
-        clean: function() {
-            var self = this;
-
-            _.each(self.pointers, function(pointer) { pointer.destroy(); });
-
-            self.pointers = {};
-        },
-
-        destroy: function(pointer) {
-            var self = this;
-
-            delete self.pointers[pointer.id];
-            pointer.destroy();
-        },
-
-        set: function(pointer, posX, posY) {
-            var self = this;
-
-            self.updateScale();
-            self.updateCamera();
-
-            var tileSize = 16 * self.scale,
-                x = ((posX - self.camera.x) * self.scale),
-                width = parseInt(pointer.element.css('width') + 24),
-                offset = (width / 2) - (tileSize / 2), y;
-
-            y = ((posY - self.camera.y) * self.scale) - tileSize;
-
-            pointer.element.css('left', (x - offset) + 'px');
-            pointer.element.css('top' , y + 'px');
-        },
-
-        setToEntity: function(entity) {
-            var self = this,
-                pointer = self.get(entity.id);
-
-            if (!pointer)
-                return;
-
-            self.set(pointer, entity.x, entity.y);
-        },
-
-        setToPosition: function(id, x, y) {
-            var self = this,
-                pointer = self.get(id);
-
-            if (!pointer)
-                return;
-
-            pointer.setPosition(x, y);
-
-            self.set(pointer, x, y);
-        },
-
-        setRelative: function(id, x, y) {
-            var self = this,
-                pointer = self.get(id);
-
-            if (!pointer)
-                return;
-
+        switch (pointer.type) {
+          case Modules.Pointers.Relative:
             var scale = self.getScale(),
-                offsetX = 0,
-                offsetY = 0;
-
-            /**
-             * Must be set in accordance to the lowest scale.
-             */
+              x = pointer.x,
+              y = pointer.y,
+              offsetX = 0,
+              offsetY = 0;
 
             if (scale === 1) {
-                offsetX = pointer.element.width() / 2 + 5;
-                offsetY = pointer.element.height() / 2 - 4;
+              offsetX = pointer.element.width() / 2 + 5;
+              offsetY = pointer.element.height() / 2 - 4;
             }
-
-            pointer.setPosition(x, y);
 
             pointer.element.css('left', (x * scale) - offsetX + 'px');
             pointer.element.css('top', (y * scale) - offsetY + 'px');
-        },
+            break;
+        }
+      });
+    },
 
-        update: function() {
-            var self = this;
+    setSize: function(element) {
+      var self = this;
+      var width = 8;
+      var height = width + (width * .2);
+      var image = 'url("img/common/hud-active.png")';
 
-            _.each(self.pointers, function(pointer) {
+      self.updateScale();
 
-                switch (pointer.type) {
-                    case Modules.Pointers.Entity:
+      element.css({
+        'width': (width * self.scale) + 'px',
+        'height': (height * self.scale) + 'px',
+        'margin': 'inherit',
+        'margin-top': '-' + ((height / 2) * self.scale) + 'px',
+        'margin-left': '1px',
+        'top': (height * self.scale) + 'px',
+        'background': image,
+        'background-size': '100% 100%',
+      });
+    },
 
-                        var entity = self.game.entities.get(pointer.id);
+    clean: function() {
+      var self = this;
 
-                        if (entity)
-                            self.setToEntity(entity);
-                        else
-                            self.destroy(pointer);
+      _.each(self.pointers, function(pointer) {
+        pointer.destroy();
+      });
 
-                        break;
+      self.pointers = {};
+    },
 
-                    case Modules.Pointers.Position:
+    destroy: function(pointer) {
+      var self = this;
 
-                        if (pointer.x !== -1 && pointer.y !== -1)
-                            self.set(pointer, pointer.x, pointer.y);
+      delete self.pointers[pointer.id];
+      pointer.destroy();
+    },
 
-                        break;
-                }
+    set: function(pointer, posX, posY) {
+      var self = this;
 
-            });
-        },
+      self.updateScale();
+      self.updateCamera();
 
-        get: function(id) {
-            var self = this;
+      var tileSize = 16 * self.scale,
+        x = ((posX - self.camera.x) * self.scale),
+        width = parseInt(pointer.element.css('width') + 24),
+        offset = (width / 2) - (tileSize / 2),
+        y;
 
-            if (id in self.pointers)
-                return self.pointers[id];
+      y = ((posY - self.camera.y) * self.scale) - tileSize;
 
-            return null;
-        },
+      pointer.element.css('left', (x - offset) + 'px');
+      pointer.element.css('top', y + 'px');
+    },
 
-        updateScale: function() {
-            this.scale = this.getDrawingScale();
+    setToEntity: function(entity) {
+      var self = this,
+        pointer = self.get(entity.id);
+
+      if (!pointer)
+        return;
+
+      self.set(pointer, entity.x, entity.y);
+    },
+
+    setToPosition: function(id, x, y) {
+      var self = this,
+        pointer = self.get(id);
+
+      if (!pointer) {
+        return;
+      }
+
+      pointer.setPosition(x, y);
+
+      self.set(pointer, x, y);
+    },
+
+    setRelative: function(id, x, y) {
+      var self = this,
+        pointer = self.get(id);
+
+      if (!pointer) {
+        return;
+      }
+
+      var scale = self.getScale(),
+        offsetX = 0,
+        offsetY = 0;
+
+      /**
+       * Must be set in accordance to the lowest scale.
+       */
+
+      if (scale === 1) {
+        offsetX = pointer.element.width() / 2 + 5;
+        offsetY = pointer.element.height() / 2 - 4;
+      }
+
+      pointer.setPosition(x, y);
+
+      pointer.element.css('left', (x * scale) - offsetX + 'px');
+      pointer.element.css('top', (y * scale) - offsetY + 'px');
+    },
+
+    update: function() {
+      var self = this;
+
+      _.each(self.pointers, function(pointer) {
+
+        switch (pointer.type) {
+          case Modules.Pointers.Entity:
+
+            var entity = self.game.entities.get(pointer.id);
+
+            if (entity) {
+              self.setToEntity(entity);
+            } else {
+              self.destroy(pointer);
+            }
+
+            break;
+
+          case Modules.Pointers.Position:
+
+            if (pointer.x !== -1 && pointer.y !== -1) {
+              self.set(pointer, pointer.x, pointer.y);
+            }
+
+            break;
+        }
+
+      });
+    },
+
+    get: function(id) {
+      var self = this;
+
+      if (id in self.pointers) {
+        return self.pointers[id];
+      }
+
+      return null;
+
+      updateScale: function() {
+          this.scale = this.getDrawingScale();
         },
 
         updateCamera: function() {
-            this.camera = this.game.renderer.camera;
+          this.camera = this.game.renderer.camera;
         },
 
         getScale: function() {
-            return this.game.getScaleFactor();
+          return this.game.getScaleFactor();
         },
 
         getDrawingScale: function() {
-            return this.game.renderer.getDrawingScale();
+          return this.game.renderer.getDrawingScale();
         }
-
     });
-
 });
